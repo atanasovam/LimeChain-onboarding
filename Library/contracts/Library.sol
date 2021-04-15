2be2901d59b7e1dc7b4424ea923b9bc998b372bb
Milena Atanasova <milena.atanasova42@gmail.com>
2021-04-15 12:51:46 +0300

    deployment prep

--- Library/contracts/Library.sol
+++ Library/contracts/Library.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity 0.6.0;
 
-import "./Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Library is Ownable {
     struct Book {
