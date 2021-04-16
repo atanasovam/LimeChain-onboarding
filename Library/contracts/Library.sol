0dc62b99a3beadfc295f682e282dadf42d8169cd
Milena Atanasova <milena.atanasova42@gmail.com>
2021-04-16 13:44:51 +0300

    code review changes

--- Library/contracts/Library.sol
+++ Library/contracts/Library.sol
@@ -8,21 +8,49 @@ contract Library is Ownable {
         string name;
         uint availableCopiesCount;
         address[] clientAddresses;
-        mapping (address => bool) clients;
+    }
+    
+    bytes32[] public allBookIDs;
+    
+    mapping (bytes32 => Book) public books;
+    mapping (address => mapping (bytes32 => bool)) borrowedBooks;
+    
+    event LogAddedBook(bytes32 id);
+    event BookBorrowed(bytes32 id);
+    event BookReturned(bytes32 id);
+    
+    modifier isNameValid(string memory _name) {
+        require(bytes(_name).length > 0, "Name shouldn't be an empty string!");
+        
+        bytes32 id = callKeccak256(_name);
+        require(books[id].availableCopiesCount == 0, "This name is already taken!");
+        _;
+    }
+    
+    modifier isCountValid(uint _count) {
+        require(_count > 0, "Count shouldn't be 0!");
+        _;
+    }
+    
+    modifier isAvailable(string memory _name) {
+        bytes32 id = callKeccak256(_name);
+        require(books[id].availableCopiesCount > 0, "This book isn't available!");
+        _;
     }
 
-    bytes32[] public allBookIDs;    
-    mapping (bytes32 => Book) public books;
+    function createBook(uint _availableCopies, string memory _name) public onlyOwner isNameValid(_name) isCountValid(_availableCopies) {
+        address[] memory addresses;
+        Book memory book = Book(_name, _availableCopies, addresses);
 
-    function createBook(uint _availableCopies, string memory _name) public onlyOwner isThisNameTaken(_name) {
-        Book memory book = Book(_name, _availableCopies, new address[](0));
         bytes32 id = callKeccak256(_name);
 
         books[id] = book;
         allBookIDs.push(id);
+        
+        emit LogAddedBook(id);
     }
     
-    function changeBookCopiesCount(string memory _name, uint _copiesCount) public onlyOwner {
+    function changeBookCopiesCount(string memory _name, uint _copiesCount) public onlyOwner isCountValid(_copiesCount) {
         bytes32 id = callKeccak256(_name);
         books[id].availableCopiesCount = _copiesCount;
     }
@@ -35,21 +63,25 @@ contract Library is Ownable {
     function borrowBook(string memory _name) public isAvailable(_name) {
         bytes32 id = callKeccak256(_name);
         
-        require(!books[id].clients[msg.sender], "Already borrowed!");
+        require(!borrowedBooks[msg.sender][id], "Already borrowed!");
 
         books[id].availableCopiesCount--;
         
-        books[id].clients[msg.sender] = true;
+        borrowedBooks[msg.sender][id] = true;
         books[id].clientAddresses.push(msg.sender);
+        
+        emit BookBorrowed(id);
     }
 
     function returnBook(string memory _name) public {
         bytes32 id = callKeccak256(_name);
         
-        require(books[id].clients[msg.sender], "Not borrowed!");
+        require(borrowedBooks[msg.sender][id], "Not borrowed!");
         
-        books[id].clients[msg.sender] = false;
+        borrowedBooks[msg.sender][id] = false;
         books[id].availableCopiesCount++;
+        
+        emit BookReturned(id);
     }
     
     function viewAllBooksCount() public view returns(uint) {
@@ -58,19 +90,5 @@ contract Library is Ownable {
     
     function callKeccak256(string memory _text) private pure returns(bytes32 result) {
       return keccak256(abi.encodePacked(_text));
-    }  
-
-    modifier isAvailable(string memory _name) {
-        bytes32 id = callKeccak256(_name);
-        require(books[id].availableCopiesCount > 0, "This book isn't available!");
-        _;
-    }
-
-    modifier isThisNameTaken(string memory _name) {
-        bytes32 id = callKeccak256(_name);
-        uint256 nameLength = bytes(books[id].name).length;
-        
-        require(nameLength <= 0, "This name is already taken!");
-        _;
     }
 }
